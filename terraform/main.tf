4e2ca9e477eaac806e1f69d5d8434eb6dd8f87db
skenguva <61749882+skenguva@users.noreply.github.com>
2024-12-15 16:59:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,7 @@
-provider "aws" {
-  region = "ap-south-1" # Change to your preferred AWS region
+provider "aws" { 
+  region     = "ap-south-1" # Change to your preferred AWS region
+  access_key = "AKIAW3MECS7446Q4KGXK" # AWS Access Key (stored securely in variables)
+  secret_key = "iv6Tg0Hadhu3AHnKiir88I2YntYs4vot2EwB/hIg" # AWS Secret Key (stored securely in variables)
 }
 
 resource "aws_instance" "example" {
