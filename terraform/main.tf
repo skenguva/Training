26aa917352339638fbd0f6f261adc24d3999544d
skenguva <61749882+skenguva@users.noreply.github.com>
2024-12-15 16:36:39 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,12 @@
 provider "aws" {
-  region = "us-east-1" # Change to your preferred AWS region
+  region = "ap-south-1" # Change to your preferred AWS region
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Replace with your region-specific AMI
+  ami           = "ami-053b12d3152c0cc71" # Replace with your region-specific AMI
   instance_type = "t2.micro"
 
   tags = {
     Name = "GitHub-Actions-Instance"
   }
 }
-
